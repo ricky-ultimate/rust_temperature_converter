c4e2253aff17a32440f21331183a53c486738089
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-04 13:37:33 +0100

    feat: define function for celcius conversion

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,8 @@ fn to_fahrenheit(temp: f64) -> f64{
     let fahrenheit = (temp * 1.8) + 32.0;
     return fahrenheit
 }
+
+fn to_celcius(temp: f64) -> f64{
+    let celcius = (temp - 32.0) * 1.8;
+    return celcius;
+}
