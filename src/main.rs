388f520b1687e4d4da0df77bf41de5b75d615b2a
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-04 16:04:45 +0100

    feat: Enhanced end logic

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,60 @@
 use std::io::{self, Write};
 
 fn main() {
-    let far = to_fahrenheit();
-    print!("{}", far)
+    display();
 }
 
 
-pub fn to_fahrenheit() -> f64{
-    println!("Enter Temperature in Celcius: ");
+pub fn to_fahrenheit(){
+    let temp = get_input("Celcius");
+    let fahrenheit = (temp * 1.8) + 32.0;
+    println!("{}C -> {}F", temp, fahrenheit );
+}
+
+pub fn to_celcius(){
+    println!();
+    let temp = get_input("Fahrenheit");
+    let celcius = (temp - 32.0) * 1.8;
+    println!("{}F -> {}C", temp, celcius );
+}
+
+pub fn get_input(name: &str) -> f64 {
+    println!();
+    print!("Enter Temperature in {} : ", name);
     io::stdout().flush().unwrap();
 
     let mut temp = String::new();
-    io::stdin().read_line(&mut temp).expect("Error");
+    io::stdin().read_line(&mut temp).expect("Invalid Message!");
 
-    let temp:f64 = temp.trim().parse().unwrap();
+    let temp: f64  = temp.trim().parse().unwrap();
 
-    let fahrenheit:f64 = (temp * 1.8) + 32.0;
-    return fahrenheit
+    return temp;
 }
 
-pub fn to_celcius(temp: f64) -> f64{
-    let celcius = (temp - 32.0) * 1.8;
-    return celcius;
+pub fn display(){
+    println!("Temperture Converter");
+    println!("----------------");
+    println!();
+    println!("1. F -> C");
+    println!("2. C -> F");
+    println!();
+    print!("Enter option (1/2): ");
+    io::stdout().flush().unwrap();
+
+    let choice = get_user_choice();
+
+    match choice {
+        1 => to_fahrenheit(),
+        2 => to_celcius(),
+        _ => {}
+    };
+
+}
+
+pub fn get_user_choice()-> u8 {
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("Error");
+
+    let choice = choice.trim().parse().unwrap();
+    return choice;
 }
