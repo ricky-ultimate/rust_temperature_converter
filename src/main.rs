46c26bb36aaa8fe7118f6b2dbf348add496f2cf1
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-05 01:37:12 +0100

    refactor: increase readability with proper whitespacing

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,48 @@
 use std::io::{self, Write};
 
-struct Fahrenheit{
-    f: f64
+struct Fahrenheit {
+    f: f64,
 }
 
-struct Celcius{
-    c: f64
+struct Celcius {
+    c: f64,
 }
 
-struct Kelvin{
-    k: f64
+struct Kelvin {
+    k: f64,
 }
 
 impl Fahrenheit {
-    fn to_celcius(&self){
-        let c: f64 = (self.f - 32.0) * (5.0/9.0);
+    fn to_celcius(&self) {
+        let c: f64 = (self.f - 32.0) * (5.0 / 9.0);
         println!("{}F -> {}C", self.f, c);
     }
 
-    fn to_kelvin(&self){
-        let k: f64 = (5.0/9.0) * (self.f + 459.67);
+    fn to_kelvin(&self) {
+        let k: f64 = (5.0 / 9.0) * (self.f + 459.67);
         println!("{}F -> {}K", self.f, k);
     }
 }
 
 impl Celcius {
-    fn to_fahrenheit(&self){
+    fn to_fahrenheit(&self) {
         let f = (self.c * 1.8) + 32.0;
         println!("{}C -> {}F", self.c, f);
     }
 
-    fn to_kelvin(&self){
+    fn to_kelvin(&self) {
         let k = self.c + 273.15;
         println!("{}C -> {}K", self.c, k);
     }
 }
 
 impl Kelvin {
-    fn to_fahrenheit(&self){
+    fn to_fahrenheit(&self) {
         let f = 1.8 * (self.k - 273.0) + 32.0;
         println!("{}K -> {}F", self.k, f);
     }
 
-    fn to_celcius(&self){
+    fn to_celcius(&self) {
         let c = self.k - 273.15;
         println!("{}K -> {}C", self.k, c);
     }
@@ -84,34 +84,34 @@ pub fn display() {
     match choice {
         1 => {
             let temp = get_input("Fahrenheit");
-            let f = Fahrenheit{f: temp};
+            let f = Fahrenheit { f: temp };
             f.to_celcius();
-        },
+        }
         2 => {
             let temp = get_input("Celcius");
-            let c = Celcius{c: temp};
+            let c = Celcius { c: temp };
             c.to_fahrenheit();
-        },
+        }
         3 => {
             let temp = get_input("Fahrenheit");
-            let f = Fahrenheit{f:temp};
+            let f = Fahrenheit { f: temp };
             f.to_kelvin();
-        },
+        }
         4 => {
             let temp = get_input("Celcius");
-            let c = Celcius{c: temp};
+            let c = Celcius { c: temp };
             c.to_kelvin();
-        },
+        }
         5 => {
             let temp = get_input("Kelvin");
-            let k = Kelvin{k: temp};
+            let k = Kelvin { k: temp };
             k.to_fahrenheit();
-        },
+        }
         6 => {
             let temp = get_input("Kelvin");
-            let k = Kelvin{k: temp};
+            let k = Kelvin { k: temp };
             k.to_celcius();
-        },
+        }
         _ => {}
     };
 }
